ef9d9dee52b45f1c9902f75b75830f8e959fc559
Ken Wagner <ken_xwagner@hotmail.com>
2015-10-20 12:01:07 -0700

    Added more features
    Polymorphism and overloading methods.

--- animalClass.py
+++ animalClass.py
@@ -1,57 +1,101 @@
 import random
 import sys
 import os
-
+ 
 class Animal:
-    __name = ""
-    __height = 0
-    __weight = 0
-    __sound = 0
-
+    # private variable start with __
+    __name = None
+    __height = None
+    __weight = None
+    __sound = None
+ 
     def __init__(self, name, height, weight, sound):
         self.__name = name
         self.__height = height
         self.__weight = weight
         self.__sound = sound
-    
+ 
     def set_name(self, name):
         self.__name = name
-
+ 
     def set_height(self, height):
         self.__height = height
-        
-    def set_weight(self, weight):
-        self.__weight = weight
-
+ 
+    def set_weight(self, height):
+        self.__height = height
+ 
     def set_sound(self, sound):
         self.__sound = sound
-        
-
-    def get__name(self):
+ 
+    def get_name(self):
         return self.__name
-
-    def get__height(self):
+ 
+    def get_height(self):
         return str(self.__height)
-
-    def get__weight(self):
+ 
+    def get_weight(self):
         return str(self.__weight)
-
-    def get__sound(self):
+ 
+    def get_sound(self):
         return self.__sound
-
+ 
     def get_type(self):
         print("Animal")
-
+ 
     def toString(self):
-        return "{} is {} cm tall and {} kilograms and say {}".format(self.__name,
-                                                                     self.__height,
-                                                                     self.__weight,
-                                                                     self.__sound)
+        return "{} is {} cm tall and {} kilograms and says {}".format(self.__name, self.__height, self.__weight, self.__sound)
+ 
+#Here's the object
+cat = Animal('Space Cat', 33, 10, 'Worship me')
 
+
+
+ 
+print(cat.toString())
 cat = Animal('Mikey', 37, 8, 'Pet Me')
+print(cat.toString())
 dog = Animal('Maddie', 81, 26, 'Throw the ball')
+print(dog.toString())
 elephant = Animal('Jumbo', 4038, 7124, 'Look out')
+print(elephant.toString())
+ 
+class Dog(Animal):
+    __owner = None
+ 
+    def __init__(self, name, height, weight, sound, owner):
+        self.__owner = owner
+        self.__animal_type = None
+ 
+   #calling super class
+        super(Dog, self).__init__(name, height, weight, sound)
+ 
+    def set_owner(self, owner):
+        self.__owner = owner
+ 
+    def get_owner(self):
+        return self.__owner
+ 
+    def get_type(self):
+        print ("Dog")
+ 
+    # Overwrite functions
+    def toString(self):
+        return "{} is {} cm tall and {} kilograms and says {}. His owner is {}".format(self.get_name(), 
+											self.get_height(), 
+											self.get_weight(), 
+											self.get_sound(), 
+											self.__owner)
+ 
+   #overloading the method
+    def multiple_sounds(self, how_many=None):
+        if how_many is None:
+            print(self.get_sound)
+        else:
+            print(self.get_sound() * how_many)
+ 
+samson = Dog("Samson", 117, 98, "Woof", "Nature")
+ 
+print(samson.toString())
 
-print (cat.toString())
-print (dog.toString())
-print (elephant.toString())
+
+                
