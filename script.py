5081e5d485bc20716ab59ed3a2920ac44ae6eea4
Ken Wagner <kenwagner@hotmail.com>
2016-03-07 15:44:42 -0800

    added monty script
    monty details

---
+++ script.py
@@ -0,0 +1,11 @@
+first_name = "Monty"
+last_name = "Python"
+full_name = first_name + " " + last_name
+year = 1969
+origin = "Presented originally in " + str(year)
+sentence = "Back in " + str(year) + ", a legend was born.\n" + "We know it now as \n\t" + first_name + " " + last_name + "\n\t\t" + "What a show!"
+
+print(full_name)
+print (origin)
+print (str(year))
+print (sentence)
